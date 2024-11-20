a80d15d4d861d6ea041653ba67e6f8df0370c324
shifakhousain <shifakhousain@gmail.com>
2024-11-20 13:43:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//Array 
+// let marks =[97 ,82, 75, 64, 36 ];
+// console.log(marks)
+// console.log(marks.length);
+
+// let heroes = ["ironman", "thor", "superman", "batman"]
+// console.log(heroes)
+// console.log(heroes.length)
+
+//for loop 
+// for (let i=0 ; i<heroes.length ; i++){
+//     console.log(heroes[i]);
+// }
+
+// for of 
+
+// for (let hero of heros){
+//     console.log(hero);
+// }
+
+// let cities = ["delhi", "pune", "hydrabad"];
+// for (let city of cities){
+//     console.log(city.toUpperCase());
+// }
+
+let foodItems = ["potato", "apple","litchi","tomato"];
+
+// foodItems.push("chips","burger","paneer");
+console.log(foodItems);
+
+foodItems.pop();
+console.log(foodItems);
+console.log(foodItems.toString());
